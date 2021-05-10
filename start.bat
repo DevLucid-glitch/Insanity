5e4ac7cfcd989a628613a286afb57f7d004f1d2d
Shiro <83925753+And9xx@users.noreply.github.com>
2021-05-11 03:10:01 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -13,10 +13,10 @@ sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant Insane:F >nul
 ICACLS C:\Windows\installer /grant Insane:F >nul
 echo Successfully Installed, Duration : 6Hours. If duration runs out please rebuild again.
-echo IP:
+echo IP :
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
-echo Username: Insane
-echo Password: Insan3@RDP
+echo Username : Insane
+echo Password : Insan3@RDP
 echo Login using the credentials above.
 echo Your RDP is ready. 2GB RAM 256GB SSD
 echo Please don't use it for mining.
