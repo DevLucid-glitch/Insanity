11d693d305a0b89d2e69a4acb599dfd1196c5d51
Shiro <83925753+And9xx@users.noreply.github.com>
2021-05-10 19:47:42 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,9 +3,9 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By Insane" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user Insane InsaneRDP /add >nul
+net user Insane Insan3RDP /add >nul
 net localgroup administrators Insane /add >nul
-net user administrator /active:yes >nul
+net user Insane /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
@@ -15,7 +15,7 @@ ICACLS C:\Windows\installer /grant Insane:F >nul
 echo Successfully Installed, Duration : 6Hours. If duration runs out please rebuild again.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
-echo Username: Admin
-echo Password: InsaneRDP
+echo Username: Insane
+echo Password: Insan3RDP
 echo Login using the credentials above.
 ping -n 10 127.0.0.1 >nul
